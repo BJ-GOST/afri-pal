0a92431e1ab0cc61bbc565c40212f2502e9c5d98
BJ-GOST <82785308+BJ-GOST@users.noreply.github.com>
2022-12-19 09:57:33 +0300

    Update settings.py

--- afripal/settings.py
+++ afripal/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-guyx7lx(gdz9#s4npxu%9zwq(_l%hf$mhkq)1^054i#p4tvqgd
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
