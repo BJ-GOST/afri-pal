3f58d0dda783e10781b87f3aa5d946f00bc78233
BJ-GOST <82785308+BJ-GOST@users.noreply.github.com>
2022-12-19 16:56:36 +0300

    Update settings.py

--- afripal/settings.py
+++ afripal/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-guyx7lx(gdz9#s4npxu%9zwq(_l%hf$mhkq)1^054i#p4tvqgd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
