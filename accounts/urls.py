190e74a9b2ab63a4077e499ddc533deeace2ab49
BJ-GOST <82785308+BJ-GOST@users.noreply.github.com>
2022-12-20 11:29:48 +0300

    adding the contacts url

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('login', views.login, name='login'),
     path('client-page', views.client_page, name='client-page'),
     path('admin-page', views.admin_page, name='admin-page'),
-    path('logout', views.logout, name='logout')
+    path('logout', views.logout, name='logout'),
+    path('contacts', views.contact_page, name='contacts')
 
 ]
